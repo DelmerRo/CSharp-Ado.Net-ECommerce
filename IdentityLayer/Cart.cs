404134d54caef612380a2102935be945e93a503a
Jindrg <jindrg@gmail.com>
2023-09-25 00:45:12 -0300

    Creando Capas de solucion --> Businnes, Data, Identity

---
+++ IdentityLayer/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityLayer
+{
+    public class Cart
+    {
+        public int IdCarrito { get; set; }
+
+        public Customer Cliente { get; set; }
+
+        public Product Producto { get; set; }
+
+        public int Cantidad { get; set; }
+    }
+}
